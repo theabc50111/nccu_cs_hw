88568695b2e7ed474d2052e4653d9912f131c0e4
YWT <theabc50111@gmail.com>
2021-11-12 19:02:19 +0800

    revise hw3 of DB

--- Database_HW/hw3/solutions/q6_check.sql
+++ Database_HW/hw3/solutions/q6_check.sql
@@ -2,6 +2,5 @@ SELECT DISTINCT p.name,p.born
 FROM public.people as p, public.crew as c ,public.titles as t 
 WHERE c.category = 'actor'
 	AND c.title_id = t.title_id
-	and c.person_id = p.person_id
-	  AND (t.premiered - p.born) <0
-
+	AND c.person_id = p.person_id
+	AND (t.premiered - p.born) <0
