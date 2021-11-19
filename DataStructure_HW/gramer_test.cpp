3b169b3f50b8d4e949b4cffad1519f94d79dd4f6
YWT <theabc50111@gmail.com>
2021-11-20 01:32:36 +0800

    revise hw2 of Data structure

--- DataStructure_HW/gramer_test.cpp
+++ DataStructure_HW/gramer_test.cpp
@@ -1,35 +1,124 @@
+// C++ code
 #include <iostream>
-#include <cstdlib>
-#include <cmath>
+#include <string>
+#include <queue>
 
-static unsigned int seed = 0; // 隨機種子
+using std::string;
+using std::cout;
+using std::endl;
 
+class BST;
+class TreeNode{
+private:
+    TreeNode *leftchild;
+    TreeNode *rightchild;
+    TreeNode *parent;
+    int key;
+    string element;
+public:
+    TreeNode():leftchild(0),rightchild(0),parent(0),key(0),element(""){};
+    TreeNode(int a, string b):leftchild(0),rightchild(0),parent(0),key(a),element(b){};
 
-bool randomVal(float threshold){
-            if (seed == 0)
-                seed = (unsigned)time(NULL);
+    int GetKey(){return key;}              // 為了在main()要能夠檢視node是否正確
+    string GetElement(){return element;}   // 才需要這兩個member function讀取private data
 
-            srand(seed);
-            float K= (float)rand()/RAND_MAX;  // 得到一個0~1 之間的數值
-            seed = rand();
-            if (K > threshold)
-                return true;
-            else
-                return false;
+    // 其餘情況, 因為class BST是class TreeNode的friend class
+    // 在class BST的member function中, 可以直接存取class TreeNode的private data
+
+    friend class BST;   // 放在 private 或 public 都可以 
 };
 
-int main() {
-    float exp = 0;
-    for (int i=1; i<=1000; i++){
-        exp += pow(0.9,i)*i;
-    }
-    std::cout << exp << std::endl;
+class BST{
+private:
+    TreeNode *root;
+    TreeNode* Leftmost(TreeNode *current);
+    TreeNode* Successor(TreeNode *current);
+public:
+    BST():root(0){};    
 
-    int a = 0;
-    while (randomVal(0.1)){
-        a += 1;
+    TreeNode* Search(int key);
+    void InsertBST(int key, string element);
+};
+
+TreeNode* BST::Search(int KEY){
+
+    TreeNode *current = root;               // 將curent指向root作為traversal起點
+
+    while (current != NULL && KEY != current->key) {  // 兩種情況跳出迴圈：
+                                                      // 1.沒找到 2.有找到
+        if (KEY < current->key){                      
+            current = current->leftchild;   // 向左走
+        }
+        else {
+            current = current->rightchild;  // 向右走
+        }
     }
-    std::cout << a << std::endl;
-    
+    return current;
+}
+
+// C++ code
+void BST::InsertBST(int key, string element){
+
+    TreeNode *y = 0;        // 準新手爸媽
+    TreeNode *x = 0;        // 哨兵
+    TreeNode *insert_node = new TreeNode(key, element);   // insert_node為將要新增的node
+
+    x = root;
+    while (x != NULL) {                 // 在while中, 以如同Search()的方式尋找適當的位置       
+        y = x;                          // y先更新到原來x的位置
+        if (insert_node->key < x->key){ // 判斷x是要往left- 還是right- 前進
+            x = x->leftchild;
+        }
+        else{
+            x = x->rightchild;
+        }
+    }                                   // 跳出迴圈後, x即為NULL
+                                        // y即為insert_node的parent
+    insert_node->parent = y;            // 將insert_node的parent pointer指向y
+
+    if (y == NULL){                     // 下面一組if-else, 把insert_node接上BST
+        this->root = insert_node;
+    }
+    else if (insert_node->key < y->key){
+        y->leftchild = insert_node;
+    }
+    else{
+        y->rightchild = insert_node;
+    }
+}
+
+int main() {
+
+    BST T;
+
+    T.InsertBST(8,"龜仙人");
+    T.InsertBST(1000,"悟空");
+    T.InsertBST(2,"克林");
+    T.InsertBST(513,"比克");
+
+
+    // cout << "Inorder Traversal:\n";
+    // T.InorderPrint();
+    // cout << endl;
+    // cout << "Level-order Traversal:\n";
+    // T.Levelorder();
+    // cout << endl << endl;
+
+    TreeNode *node = T.Search(1000);
+    if(node != NULL){
+        cout << "There is " << node->GetElement() << "(" << node->GetKey() << ")" << endl;
+    }
+    else {
+        cout << "no element with Key(1000)" << endl;
+    }
+
+    node = T.Search(73);
+    if(node != NULL){
+        cout << "There is " << node->GetElement() << "(" << node->GetKey() << ")" << endl;
+    }
+    else {
+        cout << "no element with Key(73)" << endl;
+    }
+
     return 0;
 }
