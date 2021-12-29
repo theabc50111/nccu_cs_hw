1c48caa309a3d2bf276647aaa0655ada72466a3c
YWT <theabc50111@gmail.com>
2021-12-30 02:36:19 +0800

    add hw4 of DS21

--- DataStructure_HW/gramer_test.cpp
+++ DataStructure_HW/gramer_test.cpp
@@ -3,20 +3,32 @@
 #include <bitset>
 #include <string>
 #include <random>
-
+#include <chrono>
 
 using namespace std;
 
-int main()
-{   
-    string ht = "hash table", tr = "treap", sk = "skip list", sa = "sorted array";
-    cout << ht.compare("hash table") << endl;
-    cout << tr.compare("treap") << endl;
-    cout << sk.compare("skip list") << endl;
-    cout << sa.compare("sorted array") << endl;
-    cout << ht.compare("treap") << endl;
-    cout << tr.compare("hash table") << endl;
-    cout << sk.compare("sorted array") << endl;
-    cout << sa.compare("skip list") << endl;
-    return 0;
+int main(){
+    const int n=10;  // number of experiments
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    default_random_engine generator (seed);
+    normal_distribution<double> distribution(0.0,100.0);
+
+    // specify the default value to fill the vector elements
+    int default_value = 10;
+ 
+    // Using the fill constructor to initialize a two-dimensional vector
+    // with a given default value
+    vector<vector<double>> vertex(n, vector<double>(2, default_value));
+
+    for (int i=0; i<n; ++i) {
+        double x = distribution(generator);
+        double y = distribution(generator);
+        vertex[i][0] = x;
+        vertex[i][1] = y;
+    }
+   
+
+
+  return 0;
 }
+
