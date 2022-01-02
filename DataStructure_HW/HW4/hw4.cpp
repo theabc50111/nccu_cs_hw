e3d4da31791372ed6bf28ce802620eac4745bfd1
YWT <theabc50111@gmail.com>
2022-01-02 22:45:13 +0800

    add some results of hw4 of DS21

--- DataStructure_HW/HW4/hw4.cpp
+++ DataStructure_HW/HW4/hw4.cpp
@@ -231,8 +231,7 @@ double dijkstra_arr(double graph[][NODE], int src, bool show_path_to_each) // ad
         cout<<"Vertex\t\tDistance from source vertex"<<endl;
         for(int k = 0; k<NODE; k++)                      
         { 
-            char str=65+k; 
-            cout<<str<<"\t\t\t"<<distance[k]<<endl;
+            cout<<"No." << k << " nodes\t\t\t"<<distance[k]<<endl;
         }
     }
 
@@ -290,12 +289,12 @@ void test(string file_name_ct, string file_name_ave_path, string type_record, st
 int main(){
 
     if(NODE==10 && EDGE==12){
-        test("dij_arr_10n_12e_ct.csv", "dij_arr_10n_12e_ap.csv", "dijkstra_arr", "graph_10n_12e.png");
+        test("dij_arr_10n_12e_ct.csv", "dij_arr_10n_12e_ap.csv", "dijkstra_arr", "graph_10n_12e_v2.png");
     }
     else if(NODE==100 && EDGE==200){
-        test("dij_arr_100n_200e_ct.csv", "dij_arr_100n_200e_ap.csv", "dijkstra_arr", "graph_100n_200e.png");
+        test("dij_arr_100n_200e_ct.csv", "dij_arr_100n_200e_ap.csv", "dijkstra_arr", "graph_100n_200e_v2.png");
     }
     else if(NODE==100 && EDGE==500){
-        test("dij_arr_100n_500e_ct.csv", "dij_arr_100n_500e_ap.csv", "dijkstra_arr", "graph_100n_500e.png");
+        test("dij_arr_100n_500e_ct.csv", "dij_arr_100n_500e_ap.csv", "dijkstra_arr", "graph_100n_500e_v2.png");
     }
 }
