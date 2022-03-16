87c4571cf7c502e5fc1177f564678304840cfd00
YWT <theabc50111@gmail.com>
2022-03-16 23:26:43 +0800

    add hw2 of WSM

---
+++ WebSearchMiningHW/HW2/VSM_sample/util.py
@@ -0,0 +1,20 @@
+import sys
+
+#http://www.scipy.org/
+try:
+	from numpy import dot
+	from numpy.linalg import norm
+except:
+	print("Error: Requires numpy from http://www.scipy.org/. Have you installed scipy?")
+	sys.exit() 
+
+def removeDuplicates(list):
+	""" remove duplicates from a list """
+	return set((item for item in list))
+
+
+def cosine(vector1, vector2):
+	""" related documents j and q are in the concept space by comparing the vectors :
+		cosine  = ( V1 * V2 ) / ||V1|| x ||V2|| """
+	return float(dot(vector1,vector2) / (norm(vector1) * norm(vector2)))
+
