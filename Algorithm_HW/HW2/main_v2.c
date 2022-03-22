ebc35a02322fd9acc3c5375c29ee792de6448f77
YWT <theabc50111@gmail.com>
2022-03-23 01:47:02 +0800

    revise hw2_v2 of Algo

--- Algorithm_HW/HW2/main_v2.c
+++ Algorithm_HW/HW2/main_v2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int max_con_seq(int *arr, int arr_len, int *sk_pos){
+int max_con_seq(int *arr, int arr_len){
     int global_max=0, suffix_max=0, i=0;
 
     for (i=0; i<arr_len; i++){
@@ -14,7 +14,6 @@ int max_con_seq(int *arr, int arr_len, int *sk_pos){
         }
         else{
             suffix_max=0;
-            *sk_pos = i;
         }
     } 
 
@@ -22,42 +21,24 @@ int max_con_seq(int *arr, int arr_len, int *sk_pos){
     return global_max;
 }
 
-int circle_max(int *arr, int arr_len){
-    int i, j, res, max_res=0, sk_pos=0;
-    int *p_sk_pos=&sk_pos;
-    int circle_arr[arr_len];
+int circle_max(int *arr, int arr_len, int arr_sum){
+    int i, j, res, neg_res, max_res=0;
+    int neg_arr[arr_len];
 
+    res = max_con_seq(arr, arr_len);
     for(i=0; i<arr_len; i++){
-        for(j=0; j<arr_len; j++){
-            circle_arr[j] = arr[(j+i)%arr_len];
-        }
-        res = max_con_seq(circle_arr, arr_len, p_sk_pos);
-        if (res==0) break;
-        if(max_res <= res){
-            max_res = res;
-        }
-        // for(j=1; j<arr_len; j++){
-        //     printf("%d, ", circle_arr[j]);
-        // }
-        // printf("No. %d round, max_con_seq=%d, ", i, res);
-        // printf("sk_pos= %d \n", sk_pos);
-        if (sk_pos!=0){
-            i+=sk_pos;
-        }
-        else{
-            break;
-        }
-        
+        neg_arr[i] = -1*arr[i];
     }
-
-    return max_res;
+    neg_res = max_con_seq(neg_arr, arr_len);
+    
+    return ((neg_res+arr_sum) > res) ? (neg_res+arr_sum) : res;
 }
 
 int main(){
     char buffer[1000000];
     int *arr;
     arr = malloc(1000000000000);
-    int result, arr_len=0;
+    int result, arr_len=0, arr_sum=0;
 
     if (fgets(buffer, sizeof(buffer), stdin) != 0){
         int i, n, num;
@@ -66,11 +47,12 @@ int main(){
             p += n;
             arr_len+=1;
             arr = realloc(arr, arr_len * sizeof(int));
+            arr_sum += num;
             arr[i] = num;
         }
     }
 
-    result = circle_max(arr, arr_len);
+    result = circle_max(arr, arr_len, arr_sum);
     printf("%d", result);
 
     return 0;
