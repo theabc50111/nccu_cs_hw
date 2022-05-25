8842673ee07d78286b48dd5e8af46385188e4c69
YWT <theabc50111@gmail.com>
2022-05-25 10:03:25 +0800

    add stop worker script of DisSys final project

---
+++ DistribSys_HW/Final_project/settings/stop_3_spark.sh
@@ -0,0 +1,6 @@
+export SPARK_IDENT_STRING=worker-1
+$SPARK_HOME/sbin/stop-worker.sh
+export SPARK_IDENT_STRING=worker-2
+$SPARK_HOME/sbin/stop-worker.sh
+export SPARK_IDENT_STRING=worker-3
+$SPARK_HOME/sbin/stop-worker.sh
