87885de95bb40d1df00a30d2627b5317322673ed
YWT <theabc50111@gmail.com>
2022-10-03 11:31:39 +0800

    add FTSA subject

---
+++ FinancialTimeSeriesAnalysis_HW/FTSA_example/L1_example/Lect1.R.r
@@ -0,0 +1,245 @@
+# Basic elements
+#setwd("/homoe/ywt01/Documents/Rstudio_path")
+#getwd()
+#save.image("/homoe/ywt01/Documents/Rstudio_path/FTSA.RData")
+#load("/homoe/ywt01/Documents/Rstudio_path/FTSA.RData")
+#savehistory("/homoe/ywt01/Documents/Rstudio_path/FTSA.Rhistory")
+#loadhistory("/homoe/ywt01/Documents/Rstudio_path/FTSA.Rhistory")
+
+# Vector arithmetic
+x <- c(1.8, 3.14, 4, 88.169, 13)
+length(x)
+
+# The mode of a vector
+mode(x)
+
+#Logical vectors
+x > 3.5
+
+# Character Vectors
+names(x) <- c("a", "b", "c", "d", "e")
+
+# Examples
+sys_date <- Sys.Date()
+as.integer(sys_date)
+sys_date <- Sys.Date()
+as.character(sys_date)
+
+sys_time <- Sys.time() 
+sys_time
+class(sys_time)
+as.integer(sys_time)
+
+# Vector Arithmetic
+## scalars 2 and 3 are recycled to the length of x.
+2 * x + 3
+
+##x is multiplied element-wise by the vector 5:1 (the integers from 5 through 1,) and then the vector 1:5 is added element-wise.
+(5:1) * x + (1:5) # or 5:1 * x + 1:5
+
+# application of mathematical functions.
+log(x, base=10)
+
+# Subsetting vectors
+
+## Extract elements by their index:
+x[c(1, 4)]
+
+## Exclude elements with negative index: 
+x[-c(2, 3, 5)]
+
+# Patterned vectors
+ones <- rep(1, 10)
+rep(c(2,5), c(3,2)) # repeat 2 for 3 times, repeat 5 for 2 times
+trend <- 1981:2005
+even <- seq(from = 2, to = 20, by = 2)
+seq(from = 2, to = 20, length = 5)
+c(ones, even)
+
+# More on Subsetting
+
+##by numeric indices
+x[3:5]
+
+## character subsetting can be used if there is a names attribute: 
+x[c("c", "d", "e")]
+
+#$ Logical subsetting: Selects elements corresponding to TRUE: 
+x[x > 3.5]
+
+# Lists
+mylist <- list(sample = rnorm(5), family = "normal distribution", 	parameters = list(mean = 0, sd = 1))
+mylist
+
+mylist[[1]]
+mylist[["sample"]]
+mylist$sample
+mylist[[3]]$sd
+
+## What’s the difference? 
+mylist <- list(sample = c(1,2,3,4,5))
+class(mylist[["sample"]]) 
+class(mylist["sample"])
+length(mylist[["sample"]])
+length(mylist["sample"])
+
+# Logical comparisons
+x <- c(1.8, 3.14, 4, 88.169, 13)
+y <- (x > 3 & x <= 4)
+# What happens?
+x[y]
+which(x > 3 & x <= 4)
+
+all(x > 3)
+any(x > 3)
+
+7 + TRUE
+
+all.equal(1.9 - 0.9, 1)
+identical(1.9 - 0.9, 1)
+
+# Data Input & Plotting
+tse<-read.table("~/workspace/nccu_cs_hw/FinancialTimeSeries_HW/tse.prn", header=T)
+tse<-read.table(file.choose(), header=T)
+dim(tse)
+class(tse)
+colnames(tse)
+rownames(tse)
+
+## Now, try plotting the index data
+plot.ts(tse[1:50, "JS"])
+plot.ts(tse[, "JS"])
+plot.ts(tse$JS)
+plot.ts(tse["JS"])
+plot.ts(tse[["JS"]])
+
+
+## Check data by typing, 
+tse [1:5,] #for the first 5 records, 
+## or alternatively
+head(tse) # the first 6 records
+tail(tse) # the last 6 records
+
+## How to get rid of the missing values “NA”?
+tse.dat<-na.omit(tse)
+tse.dat1<-na.exclude(tse)
+
+# Regularly-Spaced Time Series
+ts(rnorm(24), start=2012) # annual data , a time-serires from 2012~2035
+ts(rnorm(24), start=c(2012,2), frequency=4) # quarterly data , a time-serires from 2012~2018
+ts(rnorm(24), start=c(2012,3), frequency=12) # monthly data , a time-serires from 2012~2014
+
+# Date Examples
+my.date <- as.Date("1970/1/1")
+my.date
+class(my.date)
+as.numeric(my.date)
+my.date + 10
+as.numeric(my.date + 10)
+myDates = c("2013-12-19", "2003-12-20")
+as.Date(myDates)
+
+## Use the format argument to specify the input format of the date if it is not in the default format
+
+as.Date("1970/1/1"); as.Date("1970-1-1")
+as.Date("1970.1.1", format="%Y.%m.%d") 
+as.Date("1/1/1970", format="%m/%d/%Y") 
+as.Date("January 1, 1970", format="%B %d, %Y")
+as.Date("01JAN70", format="%d%b%y")
+as.Date(as.character(tse$DATE[1:5]), format="%Y%m%d")
+
+# When as.Date doesn’t Work
+as.Date("January 1, 1970", format="%B %d, %Y") 
+## and 
+as.Date("01JAN70", format="%d%b%y") 
+##both produce “NA” why?
+Sys.getlocale("LC_TIME")
+## Windows 
+Sys.setlocale(category = "LC_ALL", locale = "English")
+## Mac iOS
+Sys.setlocale(category = "LC_ALL", locale = "en_US")
+
+as.Date("2020/09/17")
+weekdays(as.Date("2020/09/17")) 
+
+as.Date("01JAN70", format="%d%b%y")  
+
+# Extracting Information from Date objects
+my.date<-as.Date("1970-01-01")
+myYear <- format(my.date, "%Y")
+as.numeric(myYear); as.character(myYear)
+
+# Covert dataframe to timeseries object
+install.packages("zoo") # if not intsalled yet
+library(zoo) # load the package
+
+tse.ts<-zoo(tse[,-1],as.Date(as.character(tse[,"DATE"]), "%Y%m%d"))
+plot(tse.ts[, "JS"])
+
+# Get All Friday Data 
+Sys.getlocale("LC_TIME", "English")
+tse<-read.table("c:/temp/tse.prn", header=T) 
+x<-as.Date(as.character(tse$DATE),"%Y%m%d")
+WD<-weekdays(x) 
+library(zoo)
+x.ts<-zoo(cbind(tse[,-1],WD), x) 
+head(x.ts)
+x.ts.Fri<-x.ts[x.ts$WD=="星期五"] # does NOT work!
+head(x.ts.Fri); tail(x.ts.Fri)
+x.ts.Fri<-x.ts[x.ts$WD=="Friday"]
+head(x.ts.Fri); tail(x.ts.Fri)
+
+# Plot Again
+library(zoo)
+tse.ts<-zoo(tse.dat[,-1], as.Date(as.character(tse.dat[,1]), format="%Y%m%d"))
+plot(tse.ts[,"JS"], main="Taiwan Stock Exchange Value-Weighted Index",col="slateblue1",ylab="TSE Index", xlab="",las=0, cex=2)
+abline(v=as.numeric(as.Date("1999/09/21")),lty=2)
+text(as.numeric(as.Date("1999/09/21")), 4000,"921 Earthquake", col="red")
+abline(h=c(min(tse.ts[,"JS"]),max(tse.ts[,"JS"])),lty=3)
+text(locator(1), "Historical High", col="red")
+text(locator(1), "Historical Low", col="red")
+
+# Multiple Plots on the Same Page
+par(mfrow=c(2,2), oma=c(1,1,2,1))
+plot(tse.ts[,"JS"], ylab="TSE Index")
+title("TSE Index")
+plot(tse.ts[,"VAL.S"], ylab="TSE Trading Value")
+title("Trading Value")
+plot(tse.ts[,"VOL.S"], ylab="TSE Trading Volume")
+title("Trading Volume")
+plot(tse.ts[,"TRA.S"], ylab="TSE # of Transaction")
+title("# of Transaction")
+mtext(outer=T,"Taiwan Stock Exchange", cex=2)
+
+# The quantmod Package
+install.packages ("quantmod") 
+# install the package, only required once  (the 1st time)
+library(quantmod) # Load the package
+getSymbols("2330.TW",from="2016-01-02", to=Sys.Date())
+TSMC<-get("2330.TW") #Avoid using numbers to start an object name.
+dim(TSMC) # dimension
+head(TSMC,3) # Show the first 3 rows
+tail(TSMC,3) # Show last 3 rows
+colnames(TSMC) #variables
+
+chartSeries(TSMC,theme="white")
+chartSeries(TSMC,theme="white")
+
+# CBOE Volatility Index: VIX from FRED
+getSymbols("VIXCLS",src="FRED")
+VIXCLS2<-window(VIXCLS, start = "2016-02-01", end = Sys.Date())
+chartSeries(VIXCLS2,theme="white")
+
+# Japan / U.S. Foreign Exchange Rate from FRED
+getSymbols("DEXJPUS",src="FRED")
+start(DEXJPUS)
+end(DEXJPUS)
+chartSeries(DEXJPUS,theme="white")
+
+
+
+
+
+
+
+  
---
+++ FinancialTimeSeriesAnalysis_HW/FTSA_example/L2_example/Lect2.R.r
@@ -0,0 +1,156 @@
+# Random Sampling
+## Drawing lottery numbers
+sample(1:49, 6)
+
+# Draw random sample from a dataset
+
+x<-sample(1:3210, 3)
+x
+
+tse[x,]
+# Draw random sample from a normal distribution
+y <- rnorm(n=100, mean=0, sd=1) 
+
+summary(y)
+
+var(y)  # variance
+
+sd(y) # standard deviation, or sqrt(var(y))
+
+hist(y) # Draw histogram
+
+plot(density(y)) # Density plot
+
+# Other common distributions: t, chisq, f, unif, binom, pois.
+
+rt(10, df=5) # Draw random # from a t-distrib. with df=5
+
+rchisq(10, df=5) # Draw random # from a chi-sq-distrib. with df=5
+
+rf(10, df1=5, df2=10) # Draw random # from an F-distrib. with df=5,10
+
+# Related functions
+qnorm(0.5) # find quantile with cumulative prob. equal to 0.5
+
+pnorm(0) # find cumulative prob. with quantile 0
+
+dnorm(0) # find density with quantile equal to 0
+
+# Finding p-value of a Statistic
+x<-seq(from=-3.5, to=3.5, by=0.01)
+y<- dnorm(x)
+plot(x, y, type="l", col = "slategray", lwd =2, main = "Density of the standard normal distribution")
+# Alternatively, curve(dnorm, from = -3.5, to = 3.5)
+text(locator(1), expression(f(x) == frac(1, sigma # sqrt(2*pi)) # e^{-frac((x - mu)^2, 2*sigma^2)}), adj = 0.5)
+abline(h=0,v=0) # Add axes
+abline(v=qnorm(c(0.05, 0.95)), lty=2)
+
+abline(v=2.2, lty=2, col=2)
+text(2.2, 0.2, "t-stat =2.2", adj=0.5)
+polygon(c( x[x>2.2], 2.2 ),  c(y[x>2.2],0 ), col="#d82521")
+Color and Math.
+
+# Writing a Function
+test.f<-function(x){
+    x^2 + 1
+     }
+ 
+test.f(x=3)
+
+# Create a function using the ¡§fix¡¨ command, e.g.
+fix(test.f). 
+
+# Plotting a Function
+x<-seq(from=-10, to=10, by=1)
+plot(x, test.f(x=x), type="l")
+abline(h=0,v=0, lty=2)
+Plotting a Function
+Plotting 2 lines on the same diagram
+x<-seq(from=-3, to=3, by=0.01)
+a.f<-function(x){x^3+3*x^2+1}
+b.f<-function(x){x^2+1}
+plot(x, a.f(x=x), type="l", ylab="Y", main="Functions on top of Each Other")
+lines(x, b.f(x=x)+0.5, lty=2, col=2)
+legend(locator(1), lty=1:2, c(expression(x^3+3*x^2+1), expression(x^2+1)), col=1:2)
+
+# Computing Asset Returns
+logret.f<-function(x){
+	n <- length(x)
+	logret <- log(x[2:n]/ x[1:(n-1)]) ¡­or log(x[-1]/ x[-n])
+	logret
+}
+
+# How about, apply this to tse.dat, and calculate the log-returns of the TSE value weighted index?
+logret.dat<-logret.f(tse.dat[,"JS"])
+plot.ts(logret.dat) 
+title("TSE Index Log-Returns")
+
+# IF Statement
+x <- c(1.8, 3.14, 4, 88.169, 13)
+if(rnorm(1) > 0) sum(x) else mean(x)
+
+# The condition cond can only be of length 1. For vectorized evaluation, use ifelse().
+ifelse(x > 4, sqrt(x), x^2)
+
+# Recursively compute first differences in the vector x.
+y <- x
+for(i in 2:5) {  y[i] <- x[i] - x[i-1]  }
+cbind(x,y)
+rbind(x,y)
+
+# Avoid such a loop whenever you can by resorting to R¡¦s vectorized operation, for example
+z <- x[-1] - x[-5]
+z
+
+# while Loop
+w <- x
+sum(w) 
+while(sum(w) < 200) {  w <- 2 # w }
+sum(w) 
+cbind(x,w)
+rbind(x,w)
+
+# R Multiplication Table
+
+for (i in 1:9){
+   for (j in 1:9){
+      cat(i, "*", j, "=", i*j, "\t")
+   }
+   cat("\n")
+}
+
+## where "\t" is similar to the "tab" key, while "\n" is similar to the ¡§carriage return¡¨ key.
+
+MTab.f<-function(i1=1, i2=9, j1=1, j2=9){
+   for (i in i1:i2){
+      for (j in j1:j2){
+         cat(i, "*", j, "=", i*j, "\t")
+      }
+   cat("\n")
+   }
+}
+
+MTab.f(i1=3, i2=5, j1=4, j2=6)
+
+# An IRR Example
+irr.f<-function(cashflow, seqn, ra=0.01, rb=0.2,rby=0.01){
+	results <- NULL
+	n <- length(cashflow)
+	for(irate in seq(ra, rb, by =rby)) {
+		total <- 0
+		for(i in 1:n) {
+			total <- total + cashflow[i]/(1 + irate)^seqn[i]
+		}
+		results <- c(results, total)
+	}
+	plot(seq(ra, rb, by =rby), results, type = "b")
+	abline(h = 0)
+	cbind(seq(ra, rb, by =rby), results)
+}
+
+irr.f(cashflow=c(-18000,rep(5600,5)), seqn=0:5,ra=0.01, rb=0.2,rby=0.01)
+
+irr.f(cashflow=c(-18000,rep(5600,5)), seqn=0:5,ra=0.16, rb=0.17,rby=0.001)
+
+irr.f(cashflow=c(-18000,rep(5600,5)), seqn=0:5,ra=0.167, rb=0.168,rby=0.0001)
+
